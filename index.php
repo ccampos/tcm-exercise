d38fbd688bbdadec313140f251bf5ced627d09fc
Claudio Campos <claudio.campos@turner.com>
2020-01-25 20:06:01 -0500

    boilerplate + initial header

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Watch Turner Classic Movies on TCM</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Playfair+Display&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <header>
+            <div>
+                <ul>
+                    <li><a href="">TCM Logo</a></li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Watch</a></li>
+                    <li><a href="">Schedule</a></li>
+                    <li><a href="">TCM in December</a></li>
+                    <li><a href="">Shop</a></li>
+                </ul>
+                <input type="search" id="site-search" name="q" aria-label="Search through TCM content">
+            </div>
+            <div>
+                <ul>
+                    <li><a href="">TCM Backlot</a></li>
+                    <li><a href="">Wine Club</a></li>
+                    <li><a href="">Fathom Events</a></li>
+                    <li><a href="">Filmstruck</a></li>
+                    <li><a href="">Bonhams</a></li>
+                    <li><a href="">TCM Tours</a></li>
+                    <li><a href="">Message Boards</a></li>
+                    <li><a href="">Film Festival</a></li>
+                    <li><a href="">Noir Alley</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1>The Wizard of Oz</h1>
+                <div>1h 37m | Aug 12, 1939 | <img src=""></div>
+            </div>
+        </header>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
