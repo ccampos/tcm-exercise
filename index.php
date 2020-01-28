2c73d5c38f670e0ebe2bf0db97525608d9c08dee
Claudio Campos <claudio.campos@turner.com>
2020-01-28 18:58:32 -0500

    start main content sections

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             <meta name="description" content="">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="css/style.css">
-            <link href="https://fonts.googleapis.com/css?family=Lato|Playfair+Display&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Lato|Playfair+Display|PT+Serif|Vollkorn&display=swap" rel="stylesheet">
             <script src="https://kit.fontawesome.com/2b33d27953.js" crossorigin="anonymous"></script>
         </head>
         <body>
@@ -18,7 +18,7 @@
                 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
             <![endif]-->
             <header>
-                <nav id="primary">
+                <nav class="primary">
                     <ul>
                         <li><img id="tcm-logo" src="images/tcm-logo-light.jpg"></li>
                         <li><a href="">Home</a></li>
@@ -51,9 +51,122 @@
                 </nav>
                 <div>
                     <h1 class="white">The Wizard of Oz</h1>
-                    <div>1h 37m | Aug 12, 1939 | <img src=""></div>
+                    <div class="emphasis"></div>
+                    <h5 class="white"><span id="film_duration">1h 37m</span>|<span id="film_date">Aug 12, 1939</span>|<span class="film_rating">PG</span></h5>
                 </div>
             </header>
+            <main>
+                <nav class="primary">
+                    <ul>
+                        <li><a href="">Overview</a></li>
+                        <li><a href="">Synopsis</a></li>
+                        <li><a href="">Credits</a></li>
+                        <li><a href="">Photos &amp; Videos</a></li>
+                        <li><a href="">Film Details</a></li>
+                        <li><a href="">Awards</a></li>
+                        <li><a href="">Articles &amp; Reviews</a></li>
+                        <li><a href="">Quotes</a></li>
+                        <li><a href="">Trivia</a></li>
+                        <li><a href="">Notes</a></li>
+                    </ul>
+                </nav>
+                <div>
+                    <section>
+                        <h2>Brief Synopsis</h2>
+                        <div class="emphasis"></div>
+                        <p>A Kansas farm girl dreams herself into a magical land where she must fight a wicked witch to escape.</p>
+                        <div class="primary"><a href="">READ MORE</a><i class="fas fa-arrow-right"></i></div>
+                    </section>
+                    <section id="cast">
+                        <h2>Cast &amp; Crew</h2>
+                        <div class="emphasis"></div>
+                        <div class="row">
+                            <div>
+                                <h4>Judy Garland</h4>
+                                <h6>Dorothy</h6>
+                            </div>
+                            <div>
+                                <h4>Frank Morgan</h4>
+                                <h6>Professor Marvel [/The Wizard of Oz/Doorkeeper of Emerald City/The coach driver/Wizard\'s doorkeeper]</h6>
+                            </div>
+                            <div>
+                                <h4>Ray Bolger</h4>
+                                <h6>"Hunk" The Scarecrow</h6>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div>
+                                <h4>Judy Garland</h4>
+                                <h6>Dorothy</h6>
+                            </div>
+                            <div>
+                                <h4>Frank Morgan</h4>
+                                <h6>Professor Marvel [/The Wizard of Oz/Doorkeeper of Emerald City/The coach driver/Wizard\'s doorkeeper]</h6>
+                            </div>
+                            <div>
+                                <h4>Ray Bolger</h4>
+                                <h6>"Hunk" The Scarecrow</h6>
+                            </div>
+                        </div>
+                    </section>
+                    <section id="photos_videos">
+                        <h2>Photos &amp; Videos</h2>
+                        <div class="emphasis"></div>
+                        <div id="container">
+                            <div id="main">
+                                <div class="overlay"></div>
+                            </div>
+                            <div>
+                                <div class="block">
+                                    <div id="top_left"></div>
+                                    <div id="top_right"></div>
+                                </div>
+                                <div class="block">
+                                    <div id="bottom_left"></div>
+                                    <div id="bottom_right"></div>
+                                </div>
+                            </div>
+                    </section>
+                    <section id="film_details">
+                        <h2>Film Details</h2>
+                        <div class="emphasis"></div>
+                        <div class="table">
+                            <div class="row">
+                                <div>MPAA Rating</div>
+                                <div><span class="film_rating">PG</span></div>
+                            </div>
+                            <div class="row">
+                                <div>Genre</div>
+                                <div>Adventure</div>
+                                <div>Family</div>
+                            </div>
+                            <div class="row">
+                                <div></div>
+                                <div>Musical</div>
+                                <div>Fantasy</div>
+                            </div>
+                            <div class="row">
+                                <div>Keywords</div>
+                                <div>Adolescents</div>
+                                <div>Charlatans</div>
+                            </div>
+                            <div class="row">
+                                <div></div>
+                                <div>Courage</div>
+                                <div>Cowardice</div>
+                            </div>
+                            <div class="row">
+                                <div>Release Date</div>
+                                <div>Aug 25, 1939</div>
+                            </div>
+                            <div class="row">
+                                <div>Premiere Information</div>
+                                <div>World Premiere in Oconomowoc, WI: 12 Aug 1939; Hollywood Premiere: 15 Aug 1939; New York Premiere: 17 Aug 1939</div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
         
         <script src="" async defer></script>
     </body>
